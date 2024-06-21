8b25479e0712c5dd67e54cbcd1572e2f2a72491d
Đinh Trung Quốc Anh <dtqanh.iugc.idea@gmail.com>
2024-06-21 11:15:48 +0700

    Update ContentView.swift

--- ToDoList_FinalProject/ContentView.swift
+++ ToDoList_FinalProject/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.managedObjectContext) private var viewContext 4
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
